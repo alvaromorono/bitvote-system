4b4a20c20292b665230e437187aed0f3fc6b8a93
alvaromorono <a.morono2999@gmail.com>
2019-08-29 18:32:20 +0200

    Adding extra dependencies

--- test/Election.js
+++
@@ -1,4 +0,0 @@
-const { expectEvent, expectRevert } = require('openzeppelin-test-helpers');
-const { shouldBehaveLikePublicRole } = require('../behaviors/access/roles/PublicRole.behavior');
-
-const { expect } = require('chai');
