e061b4d6a0dc7ce8910d688c6ac1ed92aa876db2
alvaromorono <a.morono2999@gmail.com>
2019-08-30 20:26:10 +0200

    Candidate Role Updated

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,53 +1,46 @@
 pragma solidity ^0.5.11;
 
-import "./SystemManager.sol";
 import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "./CitizenRole.sol";
+import "./CandidateRole.sol";
 
-contract Election is SystemManager, Pausable {
-    event votedEvent (address indexed _candidate, address _voter);
-    event candidateEligible(address indexed _applicant);
-    event candidateWithdrawn(address indexed _candidate);
+contract Election is CitizenRole, CandidateRole, Pausable {
+    event votedEvent (address indexed _candidate, address indexed _voter);
+    event candidateEligible(address indexed _applicant, string _addedParty);
+    event candidateWithdrawn(address indexed _candidate, string _removedParty);
 
     struct Candidate {
         address account;
         string name;
         uint voteCount;
     }
-    // Stores candidates' votes
+
     mapping(address => Candidate) public candidates;
-    // Stores candidates that have applied to be eligible
     mapping(address => bool) public applicants;
-    // Store accounts that have voted
     mapping(address => bool) public voters;
 
-
-    function vote (address _candidate) public onlyCitizen onlyCandidate whenNotPaused {
-        // require that they haven't voted before
+    function vote (address _candidate) public onlyCitizen whenNotPaused {
         require(!voters[msg.sender], "You have already voted");
-        // require candidate is eligible
         require(applicants[_candidate], "This candidate is not currently running for the election");
-        // record that the voter has voted
         voters[msg.sender] = true;
         _vote(_candidate);
     }
 
     function _vote (address _candidate) internal {
-        // update candidate vote count
         candidates[_candidate].voteCount ++;
-        // trigger voted event
         emit votedEvent(_candidate, msg.sender);
     }
 
     function standForElections(string memory _name) public onlyCandidate whenPaused {
+        require(bytes(_name).length > 0, "Enter a valid name");
         require(!applicants[msg.sender], "You have already run for elections");
         applicants[msg.sender] = true;
         _standForElections(_name);
     }
 
-    // msg.sender could show unexpected behaviour. msg.sender could be function
     function _standForElections(string memory _name) internal {
         candidates[msg.sender] = Candidate(msg.sender, _name, 0);
-        emit candidateEligible(msg.sender);
+        emit candidateEligible(msg.sender, _name);
     }
 
     function withdrawFromElections() public onlyCandidate whenPaused {
@@ -57,10 +50,11 @@ contract Election is SystemManager, Pausable {
     }
 
     function _withdrawFromElections() internal {
-        emit candidateWithdrawn(msg.sender);
+        emit candidateWithdrawn(msg.sender, candidates[msg.sender].name);
     }
 
-    function scrutiny(address _candidate) public onlyCitizen onlyCandidate whenPaused returns (uint256) {
+    function scrutiny(address _candidate) public view onlyCitizen whenPaused returns (uint256) {
+        require(applicants[_candidate], "This candidate is not currently running for the election");
         return candidates[_candidate].voteCount;
     }
 }
