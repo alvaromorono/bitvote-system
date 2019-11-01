f69a30f80d442f792827d70b5ff4f8f6a1ee4c8e
alvaromorono <a.morono2999@gmail.com>
2019-11-01 21:22:28 +0100

    Deployment Update

--- migrations/2_deploy_system.js
+++ migrations/2_deploy_system.js
@@ -1,5 +1,7 @@
 var Election = artifacts.require("./Election.sol");
+var Proposals = artifacts.require("./Proposals.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Election);
+  deployer.deploy(Proposals);
 };
